51cae064818c322071dde723b7ce0b4a2322ca18
Michael Truong <michael.vn.truong@gmail.com>
2024-07-10 17:31:21 +0100

    Add MVC to C# template

--- csharp-app/Search/Program.cs
+++ csharp-app/Search/Program.cs
@@ -1,9 +1,13 @@
 WebApplicationBuilder builder = WebApplication.CreateBuilder(args);
 
+builder.Services.AddControllers();
+
 WebApplication app = builder.Build();
 
 string json = File.ReadAllText("Stubs/parts.json");
 
 app.MapGet("/hello", () => json);
+
+app.MapControllers();
 
 app.Run();
