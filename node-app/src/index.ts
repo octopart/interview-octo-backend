7591a2b1fc833a9af0fd9b8f8a7ee04e31bb3ae2
Kai Leddy <epickaimail@gmail.com>
2026-02-24 16:36:21 +0000

    minor updates to match the shape of the python boilerplate

--- node-app/src/index.ts
+++ node-app/src/index.ts
@@ -1,10 +1,11 @@
 import express from "express";
+import { partOffers } from "#utils/jsondata.js";
 
 const app = express();
 const port = "3000";
 
-app.get("/", (req, res) => {
-  res.send("Hello World!");
+app.get("/health", (req, res) => {
+  res.send("OK");
 });
 
 app.listen(port, () => {
